cd969fa5a6efcb7eeb595a123653090d80aeef62
Martina Selldén <martina.sellden@gmail.com>
2022-10-06 14:52:24 +0200

    First version

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+interface ICharacter
+{
+    string Name { get; set;}
+    int Hp { get; set;}
+
+    void Speak();
+    void Move();
+}
+
+class Player : ICharacter
+{
+    public string Name { get; set; }
+
+    public int Hp { get; set; }
+
+    public void Speak()
+    {
+        throw new NotImplementedException();
+    }
+    public void Move()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Player(string name)
+    {
+       this.Name = name;
+       this.Hp = 100;
+    }
+
+
+}
+
+class Monster : ICharacter
+{
+    public string Name { get; set; }
+
+    public int Hp { get; set; }
+
+    public void Speak()
+    {
+        throw new NotImplementedException();
+    }
+    public void Move()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Monster(string name)
+    {
+        this.Name = name;
+        this.Hp = 100;
+    }
+
+}

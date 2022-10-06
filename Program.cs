24807ff5e19793bc32336966679757584381f6ed
Martina Selldén <martina.sellden@gmail.com>
2022-10-06 21:34:22 +0200

    Interface ICharacter, classes Player, Monster, Interaction

--- Program.cs
+++ Program.cs
@@ -1,23 +1,26 @@
 ﻿
+Player Player = new Player("Hero");
+Monster Monster = new Monster("Monster");
+
+Interaction Interaction = new Interaction();
+Interaction.Fight(Player, Monster);
+
+
 interface ICharacter
 {
-    string Name { get; set;}
-    int Hp { get; set;}
+    string Name { get; set; }
+    int Hp { get; set; }
 
     void Speak();
     void Move();
 
-    void Fight();
-
 }
-
 
 class Player : ICharacter
 {
     public string Name { get; set; }
 
     public int Hp { get; set; }
-
     public void Speak()
     {
         Console.WriteLine("I'm a hero!");
@@ -39,10 +42,9 @@ class Player : ICharacter
 
     public Player(string name)
     {
-       this.Name = name;
-       this.Hp = 100;
+        this.Name = name;
+        this.Hp = 100;
     }
-
 
 }
 
@@ -58,22 +60,7 @@ class Monster : ICharacter
     }
     public void Move()
     {
-        Console.WriteLine("Sneaking around waiting for prey...");    
-    }
-
-    public void Fight()
-    {
-
-    }
-
-    int Damage()
-    {
-        int damage;
-
-        Random random = new Random();
-        damage = random.Next(0, 20);
-
-        return damage;
+        Console.WriteLine("Sneaking around waiting for prey...");
     }
 
     public Monster(string name)
@@ -82,4 +69,43 @@ class Monster : ICharacter
         this.Hp = 100;
     }
 
+}
+
+class Interaction
+{
+    public void Fight(ICharacter opponent1, ICharacter opponent2)
+    {
+        Console.WriteLine("The fight is on!");
+
+        int opponent1Hp = opponent1.Hp;
+        int opponent2Hp = opponent2.Hp;
+
+        while (opponent1Hp > 0 && opponent2Hp > 0)
+        {
+            int opponent1Damage = Damage();
+            int opponent2Damage = Damage();
+
+            if (opponent1Damage > opponent2Damage)
+                opponent2Hp -= opponent1Damage;
+
+            else
+                opponent1Hp -= opponent2Damage;
+        }
+
+        if (opponent1Hp > opponent2Hp)
+            Console.WriteLine(opponent1 + " is victorious!");
+
+        else
+            Console.WriteLine(opponent2 + " is victorious!");
+    }
+
+    protected int Damage()
+    {
+        int damage;
+
+        Random random = new Random();
+        damage = random.Next(10, 40);
+
+        return damage;
+    }
 }
